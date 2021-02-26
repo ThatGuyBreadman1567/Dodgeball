9dcf4c6a295ec067867dfccf9afacd6aff19dcce
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2021-02-26 09:19:46 -0500

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -40,9 +40,7 @@ public class Ball extends JComponent
 	{
 		Graphics2D g2 = (Graphics2D) g;
 		
-		g2.setColor(Color.YELLOW);
+		g2.setColor(Color.RED);
 		g2.fill(circle);
-		g2.setColor(Color.BLACK);
-		g2.draw(circle);
 	}
 }
